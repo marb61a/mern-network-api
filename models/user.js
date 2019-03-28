3044bf8977687ccf3b45980a78afe49a70c31007
marb61a <marb61a@gmail.com>
2019-03-28 20:48:21 +0000

    updated user model and routes

--- models/user.js
+++ models/user.js
@@ -29,5 +29,27 @@ const userSchema = new mongoose.Schema({
   about: {
     type: String,
     trim: true
+  },
+  role: {
+    type: String,
+    default: "subscriber"
   }
-})
+});
+
+// UserSchema methods
+userSchema.methods = {
+  authenticate: function(plainText) {
+
+  },
+  encryptPassword: function(password) {
+    if(!password) return "";
+
+    try {
+
+    } catch {
+      return "";
+    }
+  }
+}
+
+module.exports = mongoose.model("User", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const {
+  allUsers
+} = require('../controllers/user');
 const router = express.Router();
+
+router.get("/users", allUsers);
 
 module.exports = router;
