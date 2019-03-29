accfa62ad948ef93357ac4646861ddf462996682
marb61a <marb61a@gmail.com>
2019-03-29 20:47:30 +0000

    added initial post routes and controller

---
+++ controllers/post.js
@@ -0,0 +1,16 @@
+const Post = require("../models/post");
+const formidable = require("formidable");
+const fs = require("fs");
+const _ = require("lodash");
+
+exports.postById = (req, res, next, id) => {
+
+};
+
+exports.getPosts = async(req, res) => {
+
+}
+
+exports.createPost = (req, res, next) => {
+  
+}
