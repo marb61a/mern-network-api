ad67a0ae104c8aae43ecfbd427fb61817711e8b0
marb61a <marb61a@gmail.com>
2019-03-31 11:30:39 +0100

    updated app.js

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const fs = require("fs");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+    .connect()
+    .then(() => { console.log("DB Connected")});
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+// Import routes
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 // Middleware
 app.use(morgan("dev"));
@@ -11,6 +29,14 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use(function(err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).json({ error: "Unauthorized!" });
+    }
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
