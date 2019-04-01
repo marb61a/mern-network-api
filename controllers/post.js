c626e7dae820dc86169ba4f9b9d9fc2f5ef0e736
marb61a <marb61a@gmail.com>
2019-04-01 12:08:37 +0100

    added createPost method to post controller

--- controllers/post.js
+++ controllers/post.js
@@ -12,5 +12,33 @@ exports.getPosts = async(req, res) => {
 }
 
 exports.createPost = (req, res, next) => {
-  
-}
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if(err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded"
+      });
+    }
+
+    let post = new Post();
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    post.postedBy = req.profile;
+
+    if(files.photo) {
+      post.photo.data = fs.readFileSync(files.photo.path);
+      post.photo.contentType = files.photo.type;
+    }
+
+    post.save((err, result) => {
+      if(err) {
+        return res.status(400).json({
+          error: err
+        });
+      }
+
+      res.json(result);
+    });
+  });
+};
