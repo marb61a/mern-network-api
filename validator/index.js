2e5bd624226c155911b0fb3ea65c96321b351cfe
marb61a <marb61a@gmail.com>
2019-04-01 12:17:39 +0100

    added creatPostValidator

---
+++ validator/index.js
@@ -0,0 +1,24 @@
+exports.createPostValidator = (req, res, next) => {
+  // Title
+  req.check("title", "Create a title").notEmpty();
+  req.check("title", "Title must be between 4 to 150 characters")
+    .isLength({ min: 4, max: 150});
+  
+  // Body
+  req.check("body", "Create a body").notEmpty();
+  req.check("body", "The body must be between 4 to 150 characters")
+    .isLength({ min: 4, max: 2000});
+  
+  // Error Checking
+  const errors = req.validationErrors();
+  // If there are errors show them starting with the first
+  if(errors) {
+    const firstError = errors.map(error => error.msg)[0];
+    return res.status(400).json({ 
+      error: firstError 
+    });
+  }
+
+  // Proceed to next middleware
+  next();
+}
