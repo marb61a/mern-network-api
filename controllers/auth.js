b16cd9c4d531b8140c8f75c05502514e27410df8
marb61a <marb61a@gmail.com>
2019-04-03 11:56:32 +0100

    added signup function to auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,29 @@ require("dotenv").config();
 const expressJwt = require("express-jwt");
 const User = require("../models/user");
 const _ = require("lodash");
+const { sendEmail } = require("../helpers");
 
 exports.signup = async(req, res) => {
-  console.log("hello");
-}
+  const userExists = await User.findOne({
+    email: req.body.email
+  });
+  if(userExists) {
+    return res.status(403).json({
+      error: "Email is already taken!"
+    });
+  }
+
+  const user = await new User(req.body);
+  await user.save();
+  res.status(200).json({
+    message: "Signup is successful! Please login."
+  });
+};
+
+exports.signin = (req, res) => {
+  // Find the user based on their email address
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+
+  });
+};
