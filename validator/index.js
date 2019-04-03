817a06eddaa39f55948276a0a7db9e02c94eecf1
marb61a <marb61a@gmail.com>
2019-04-03 12:23:37 +0100

    added userSignupValidator function to validators index file

--- validator/index.js
+++ validator/index.js
@@ -22,3 +22,38 @@ exports.createPostValidator = (req, res, next) => {
   // Proceed to next middleware
   next();
 }
+
+exports.userSignupValidator = (req, res, next) => {
+  // Check name is not null and is between 4 and 10 characters
+  req.check("name", "Name is required").notEmpty();
+
+  // Check that the email address is not null, is valid and is normalised
+  req.check("email", "Email must be between 3 to 32 characters")
+    .matches(/.+\@.+\..+/)
+    .withMessage("Email address must contain @")
+    .isLength({
+      min: 4,
+      max: 2000
+    });
+    
+    // Check for password & check password
+    req.check("password", "Password is required").notEmpty();
+    req.check("password")
+      .isLength({min: 6})
+      .withMessage("Password must contain at least 6 characters")
+      .matches(/\d/)
+      .withMessage("Password must contain a number");
+
+    // Error Checking
+    const errors = req.validationErrors();
+    // If there are errors show them starting with the first
+    if(errors) {
+      const firstError = errors.map(error => error.msg)[0];
+      return res.status(400).json({ 
+        error: firstError 
+      });
+    }
+
+    // Proceed to next middleware
+    next();
+};
