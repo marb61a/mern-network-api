c622b54010548fd31c4c09e9cf5d42066ddb2d1a
marb61a <marb61a@gmail.com>
2019-04-05 20:33:05 +0100

    updated user controller

--- controllers/user.js
+++ controllers/user.js
@@ -6,9 +6,20 @@ const fs = require("fs");
 exports.userById = (req, res, next, id) => {
   User.findById(id)
     .exec((err, user) => {
+      if(err || !user) {
+        return res.status(400).json({
+          error: "User not found"
+        });
+      }
 
+      req.profile = user;
+      next();
     });
-}
+};
+
+exports.hasAuthorization = (req, res, next) => {
+
+};
 
 exports.allUsers = (req, res) => {
   User.find((err, users) => {
@@ -21,4 +32,23 @@ exports.allUsers = (req, res) => {
     res.json(users);
   })
     .select("name email updated created role");
-}
+};
+
+exports.getUser = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
+
+exports.updateUser = (req, res, next) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, files) => {
+    if(err) {
+      return res.status(400).json({
+
+      });
+    }
+  });
+};
