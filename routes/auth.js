618b4fa468815628c2590a4132c7133e6bd288ce
marb61a <marb61a@gmail.com>
2019-04-07 20:38:58 +0100

    added routes to post and auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const { signup } = require("../controllers/auth");
+const { 
+  signup,
+  signin,
+  signout
+} = require("../controllers/auth");
+
+const { userById } = require("../controllers/user");
 
 const router = express.Router();
+
+router.post("/signup", userSignupValidator, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
+
+// If any route contains :userId, the app will first execute userById
+router.param("userId", userById);
 
 module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -2,15 +2,28 @@ const express = require("express");
 const { 
   getPosts, 
   createPost,
-  postById
+  postsByUser,
+  postById,
+  updatePost,
+  deletePost,
+  photo,
 } = require("../controllers/post");
+
+const { requireSignin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
+const { createPostValidator } = require("../validator");
 
 const router = express.Router();
 
 router.get("/posts", getPosts);
 
 // Post routes
-router.post(createPost);
+router.post(
+  "/post/new/:userId",
+  requireSignin,
+  createPost,
+  createPostValidator
+);
 router.param("postId", postById);
 
 module.exports = router;
