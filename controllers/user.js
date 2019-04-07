0780f6d737d89b618dfc702f3b5524cb694b3e56
marb61a <marb61a@gmail.com>
2019-04-07 11:29:00 +0100

    added updateUser function to user controller

--- controllers/user.js
+++ controllers/user.js
@@ -47,8 +47,39 @@ exports.updateUser = (req, res, next) => {
   form.parse(req, (err, fields, files) => {
     if(err) {
       return res.status(400).json({
-
+        error: "A photo could not be uploaded"
       });
     }
+
+    // Save user
+    let user = req.profile;
+    user = _.extend(user, fields);
+    user.updated = Date.now();
+
+    if(files.photo) {
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contentType = files.photo.type;
+    }
+
+    user.save((err, result) => {
+      if(err) {
+        return res.status(400).json({
+          error: err
+        });
+      }
+
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json(user);
+    });
   });
+};
+
+exports.userPhoto = (req, res, next) => {
+  if (req.profile.photo.data) {
+    res.set(("Content-Type", req.profile.photo.contentType));
+    return res.send(req.profile.photo.data);
+  }
+
+  next();
 };
