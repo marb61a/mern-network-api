21c5ceb40767620da98f84aabfcd69b067e374dc
marb61a <marb61a@gmail.com>
2019-04-12 20:12:56 +0100

    added like and unlike functions to post controller

--- controllers/post.js
+++ controllers/post.js
@@ -171,3 +171,39 @@ exports.photo = (req, res, next) => {
 exports.singlePost = (req, res) => {
   return res.json(req.post);
 };
+
+exports.like = (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    { $push: { likes: req.body.userId } },
+    { new: true }
+  )
+    .exec((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      } else {
+        res.json(result);
+      }
+
+  });
+};
+
+exports.unlike = (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    { $pull: { likes: req.body.userId } },
+    { new: true }
+  )
+    .exec((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      } else {
+        res.json(result);
+      }
+      
+    });
+}
